ee7d0e3cec8e9cb43b8e3757e9a19dab4379a319
Filipstrozik <filipshelby@gmail.com>
2023-03-12 09:48:56 +0100

    add lab03 preparation

---
+++ lab03/grpcGreeterClient/grpcGreeterClient/Program.cs
@@ -0,0 +1,17 @@
+﻿using System.Threading.Tasks;
+using Grpc.Net.Client;
+using grpcGreeterClient.Services;
+using GrpcGreeterClient;
+
+MyData.Info();
+
+using var channel = GrpcChannel.ForAddress("https://localhost:7019");
+var client = new Greeter.GreeterClient(channel);
+var reply = await client.SayHelloAsync(
+        new HelloRequest { Name = "GreeterClient" });
+Console.WriteLine("Greetings: " + reply.Message);
+Console.WriteLine("Press any key to exit");
+Console.ReadKey();
+
+
+// See https://aka.ms/new-console-template for more information
