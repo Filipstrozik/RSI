be5344ae22f33b348c874f015d6ea15d43b510eb
Filipstrozik <filipshelby@gmail.com>
2023-03-18 14:02:21 +0100

    lab4

--- lab03/grpcGreeterClient/grpcGreeterClient/Program.cs
+++ lab03/grpcGreeterClient/grpcGreeterClient/Program.cs
@@ -5,7 +5,8 @@ using GrpcGreeterClient;
 
 MyData.Info();
 
-using var channel = GrpcChannel.ForAddress("http://172.20.10.2:7019");
+//using var channel = GrpcChannel.ForAddress("http://172.20.10.2:7019");
+using var channel = GrpcChannel.ForAddress("http://localhost:7019");
 var client = new Greeter.GreeterClient(channel);
 var reply = await client.SayHelloAsync(
         new HelloRequest { Name = "GreeterClient" });
