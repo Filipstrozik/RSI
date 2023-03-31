e0469801de364514de03f1cf19dca56d79566d38
Piotr Grygoruk <grygorukpp@gmail.com>
2023-03-31 14:16:53 +0200

    menu working

--- lab05/GrpcStreamingClient/GrpcStreamingClient/Program.cs
+++ lab05/GrpcStreamingClient/GrpcStreamingClient/Program.cs
@@ -7,46 +7,59 @@ var client = new ImageService.ImageServiceClient(channel);
 // Open the file for reading
 
 MyData.Info();
-Console.WriteLine("Press any key to upload");
-Console.ReadKey();
-using (var fileStream = File.OpenRead(@"..\..\..\Files\test.png"))
+
+Console.WriteLine("Wybierz opcję");
+Console.WriteLine("1 - Wysyłanie pliku");
+Console.WriteLine("2 - Pobieranie pliku");
+string choice = Console.ReadLine();
+
+Console.WriteLine("Podaj scieżkę do pliku");
+string file = Console.ReadLine();
+
+switch (choice)
 {
-    // Create a stream of ImageData objects
-    using (var call = client.StreamToServer())
-    {
-        // Stream the file data to the server
-        byte[] buffer = new byte[256];
-        int bytesRead;
-        while ((bytesRead = await fileStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+    case "1":
+        using (var fileStream = File.OpenRead(@"..\..\..\Files\test.png"))
         {
-            await call.RequestStream.WriteAsync(new ImageData { Data = Google.Protobuf.ByteString.CopyFrom(buffer, 0, bytesRead) });
+            // Create a stream of ImageData objects
+            using (var call = client.StreamToServer())
+            {
+                // Stream the file data to the server
+                byte[] buffer = new byte[256];
+                int bytesRead;
+                while ((bytesRead = await fileStream.ReadAsync(buffer, 0, buffer.Length)) > 0)
+                {
+                    await call.RequestStream.WriteAsync(new ImageData { Data = Google.Protobuf.ByteString.CopyFrom(buffer, 0, bytesRead) });
+                }
+                await call.RequestStream.CompleteAsync();
+
+                // Wait for the server to finish processing the stream
+                await call.ResponseAsync;
+            }
         }
-        await call.RequestStream.CompleteAsync();
-
-        // Wait for the server to finish processing the stream
-        await call.ResponseAsync;
-    }
-}
-
-Console.WriteLine("Press any key to download");
-Console.ReadKey();
-
-using (var call = client.StreamToClient(new Google.Protobuf.WellKnownTypes.Empty()))
-{
-    // Create a file stream to write the received data
-    using (var fileStream = File.Create(@"..\..\..\Recieved\received.png"))
-    {
-        // Read data from the server stream and write it to the file stream
-        while (await call.ResponseStream.MoveNext())
+        Console.WriteLine("Plik wysłany poprawnie");
+        break;
+    case "2":
+        using (var call = client.StreamToClient(new Google.Protobuf.WellKnownTypes.Empty()))
         {
-            ImageData imageData = call.ResponseStream.Current;
-            await fileStream.WriteAsync(imageData.Data.ToByteArray());
-            Console.WriteLine(imageData);
+            // Create a file stream to write the received data
+            using (var fileStream = File.Create(@"..\..\..\Recieved\received.png"))
+            {
+                // Read data from the server stream and write it to the file stream
+                while (await call.ResponseStream.MoveNext())
+                {
+                    ImageData imageData = call.ResponseStream.Current;
+                    await fileStream.WriteAsync(imageData.Data.ToByteArray());
+                    Console.WriteLine(imageData);
+                }
+            }
         }
-    }
+        Console.WriteLine("Plik pobrany poprawnie");
+        break;
+    default:
+        Console.WriteLine("Niepoprawny wybór");
+        break;
 }
-
-Console.WriteLine("File downloaded successfully");
 
 // Shutdown the gRPC channel
 await channel.ShutdownAsync();
