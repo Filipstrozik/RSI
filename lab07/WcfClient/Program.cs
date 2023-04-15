76e58973d063fbfe970a9ac76d8f837d72d61ef1
Filipstrozik <filipshelby@gmail.com>
2023-04-15 18:19:53 +0200

    fixxed service reference namespace

--- lab07/WcfClient/Program.cs
+++ lab07/WcfClient/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.ServiceModel;
 using System.Text;
 using System.Threading.Tasks;
-using WcfClient.ServiceReference1;
+using WcfClient.WcfService;
 
 namespace WcfClient
 {
@@ -13,14 +13,14 @@ namespace WcfClient
         static void Main(string[] args)
         {
             CalculatorClient client1 = new CalculatorClient("BasicHttpBinding_ICalculator");
-
+            int result = 0;
             try
             {
-                int n1 = 2147483647;
-                int n2 = 2147483647;
+                int n1 = 3;
+                int n2 = 2;
 
-                int result = client1.iMul(n1, n2);
-                Console.WriteLine($"{n1} / {n2} = {result}");
+                result = client1.iMul(n1, n2);
+                Console.WriteLine($"{n1} * {n2} = {result}");
             }
             catch (FaultException<DivideByZeroException> ex)
             {
@@ -36,6 +36,12 @@ namespace WcfClient
             }
 
             client1.Close();
+
+            CalculatorClient client2 = new CalculatorClient("WSHttpBinding_ICalculator");
+            Console.WriteLine("...calling Multiply (for endpoint2) - ");
+            result = client2.iMul(-3, 9); //just example values
+            Console.WriteLine("Result = " + result);
+            client2.Close();
             Console.ReadKey();
 
         }
