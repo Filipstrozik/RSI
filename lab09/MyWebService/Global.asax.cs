3ac7229b7b111c2daeae8e758a874a5f3d1b0329
Filipstrozik <filipshelby@gmail.com>
2023-05-08 22:03:39 +0200

    fixed global file

--- lab09/MyWebService/Global.asax.cs
+++ lab09/MyWebService/Global.asax.cs
@@ -4,37 +4,35 @@ using System.Web;
 
 namespace MyWebService
 {
-    public class Global : System.Web.HttpApplication
+    public class Global : HttpApplication
     {
-        protected void Application_Start(object sender, EventArgs e) 
+        protected void Application_Start(object sender, EventArgs e)
         {
-            Debug.Print("AAAAAAAAAAAAAAAAAAAAAAAAAAAAAAAA");
             MyData.Info();
         }
-        protected void Session_Start(object sender, EventArgs e) {
-            Debug.Print("CCCCCCCCCCCCCCCCCCCCCCA");
+        protected void Session_Start(object sender, EventArgs e)
+        {
         }
 
         protected void Application_BeginRequest(object sender, EventArgs e)
         {
-            Debug.Print("BBBBBBBBBBBBBBBBB");
-            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin","*");
+            HttpContext.Current.Response.AddHeader("Access-Control-Allow-Origin", "*");
             if (HttpContext.Current.Request.HttpMethod == "OPTIONS")
             {
                 HttpContext.Current.Response.AddHeader(
                 "Access-Control-Allow-Methods",
-                "POST, PUT, DELETE"); 
-               
+                "POST, PUT, DELETE");
+
                 HttpContext.Current.Response.AddHeader(
                 "Access-Control-Allow-Headers",
-                "Content-Type, Accept"); 
-               
+                "Content-Type, Accept");
+
                 HttpContext.Current.Response.AddHeader(
                 "Access-Control-Max-Age", "7200");
                 HttpContext.Current.Response.End();
             }
         }
-        protected void Application_AuthenticateRequest(object sender,EventArgs e) { }
+        protected void Application_AuthenticateRequest(object sender, EventArgs e) { }
         protected void Application_Error(object sender, EventArgs e) { }
         protected void Session_End(object sender, EventArgs e) { }
         protected void Application_End(object sender, EventArgs e) { }
