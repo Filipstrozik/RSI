24aa7e00559e2f1ca486b15ec4aeb0714992e14c
Filipstrozik <filipshelby@gmail.com>
2023-06-14 23:43:46 +0200

    edit form works

--- projekt/front-fptodo/src/app/components/board/board.component.ts
+++ projekt/front-fptodo/src/app/components/board/board.component.ts
@@ -1,11 +1,23 @@
 import { Component, Input } from '@angular/core';
 import Board from 'src/app/models/board';
+import { TodoapiService } from 'src/app/services/todoapi.service';
 
 @Component({
   selector: 'app-board',
   templateUrl: './board.component.html',
-  styleUrls: ['./board.component.css']
+  styleUrls: ['./board.component.css'],
 })
 export class BoardComponent {
   @Input() board!: Board;
+
+  constructor(private todoApiService: TodoapiService) {}
+
+  updateBoard() {
+    console.log('update board');
+    this.todoApiService
+      .getBoardById(this.board.id)
+      .subscribe((board: Board) => {
+        this.board = board;
+      });
+  }
 }
