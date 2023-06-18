50f65d7f165a6ef8140a83096ab167e8c09e8857
Piotr Grygoruk <grygorukpp@gmail.com>
2023-06-18 16:10:48 +0200

    validation

--- projekt/RSIapi/Models/ToDoItem.cs
+++ projekt/RSIapi/Models/ToDoItem.cs
@@ -4,19 +4,24 @@ namespace RSIapi.Models
     public class ToDoItem
     {
         public int Id { get; set; }
+
         [Required]
         [MaxLength(50)]
         public string Name { get; set; }
+
+        [Required]
         public bool IsComplete { get; set; } = false;
 
         private DateTime _dueTime;
 
+        [Required]
         public DateTime DueTime
         {
             get { return _dueTime.ToUniversalTime(); }
             set { _dueTime = value.ToLocalTime(); }
         }
 
+        [Required]
         [Range(1, 5)]
         public int Priority { get; set; } = 3;
 
--- projekt/RSIapi/Models/User.cs
+++ projekt/RSIapi/Models/User.cs
@@ -4,11 +4,19 @@ namespace RSIapi.Models
     public class User
     {
         public int Id { get; set; }
+
         [Required]
         [MaxLength(50)]
         public string Name { get; set; }
+
+        [Required]
+        [MaxLength(50)]
         public string Email { get; set; }
+
+        [Required]
+        [Range(1, 100)]
         public int Age { get; set; }
+
         public ICollection<ToDoItem> ToDoItems { get; } = new List<ToDoItem>();
     }
 }
